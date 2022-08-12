fe5615d3b731786a16b27d6b44ac7a2349b72627
feresrtas <feresrtas.gmail.com>
2022-08-12 21:18:40 +0300

    sonhali

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,81 +1,59 @@
-import nBlog from '../assets/blok.png'
-
-const BlogForm = ({blog, setBlog, handleSubmit}) => {
-
+const BlogForm = ({ blog, setBlog, handleSubmit }) => {
   const handleChange = (e) => {
-    e.preventDefault()
-    const {id, value} = e.target;
+    e.preventDefault();
+    const { id, value } = e.target;
     setBlog({ ...blog, [id]: value });
-    console.log(blog)
-  }
+  };
 
   return (
-    <div>
-      <div className="card bg-dark text-white">
-        <img
-          src={'https://picsum.photos/800/800'}
-          className="card-img"
-          alt="samp"
-        />
-        <div className="card-img-overlay register-form p-5 w-75">
-          <div className="d-flex">
-          <h1 className="form-title display-3">Add_New_Blog</h1>
-          <img src={nBlog} alt="blokimg" width='75px'/>
-          </div>
-          <form id="addblog" onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label className="form-label">
-                Title
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="title"
-                placeholder="Enter a title"
-                required
-                value={blog.title}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">
-                Image URL
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="imgurl"
-                placeholder="Enter your image URL"
-                required
-                value={blog.imgurl}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">
-                Content
-              </label>
-              <textarea
-                type="text"
-                className="form-control"
-                rows="6"
-                id="content"
-                placeholder="Enter your content"
-                required
-                value={blog.content}
-                onChange={handleChange}
-              />
-            </div>
-            <input
-              type="submit"
-              className="btn btn-primary form-control"
-              value="Add"
-            />
-          </form>
+    <div className="d-flex px-5">
+      <form onSubmit={handleSubmit}>
+        <h1 className="form-title display-4">Add New Blog</h1>
+        <div className="mb-3">
+          <label className="form-label">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="Enter a title"
+            required
+            value={blog.title}
+            onChange={handleChange}
+          />
         </div>
-      </div>
+        <div className="mb-3">
+          <label className="form-label">Image URL</label>
+          <input
+            type="text"
+            className="form-control"
+            id="imgurl"
+            placeholder="Enter your image URL"
+            required
+            value={blog.imgurl}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Content</label>
+          <textarea
+            type="text"
+            className="form-control"
+            rows="6"
+            id="content"
+            placeholder="Enter your content"
+            required
+            value={blog.content}
+            onChange={handleChange}
+          />
+        </div>
+        <input
+          type="submit"
+          className="btn btn-primary form-control"
+          value="Add"
+        />
+      </form>
     </div>
   );
-}
+};
 
 export default BlogForm;
--- src/pages/UpdateBlog.jsx
+++ src/pages/UpdateBlog.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { UpBlog, GetBlog } from '../helpers/firebase';
+import { toastSuccessNotify } from '../helpers/toastNotify';
+import { useParams } from 'react-router-dom';
+// import { BlogContext } from '../contexts/BlogContext';
+// import { details } from './Details';
+function UpdateBlog() {
 
-const UpdateBlog = () => {
+  const { id } = useParams();
+  const { blogList } = GetBlog();
+  const edit = blogList?.find?.(blog => blog.id === id);
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    UpBlog(edit)
+    toastSuccessNotify('Edited')
+    navigate('/')
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>UpdateBlog</div>
-  )
+    <div className="d-flex px-5">
+      <form onSubmit={handleSubmit}>
+        <h1 className="form-title display-4">Edit Blog</h1>
+        <div className="mb-3">
+          <label className="form-label">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="Enter a title"
+            required
+            value={edit?.title}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Image URL</label>
+          <input
+            type="text"
+            className="form-control"
+            id="imgurl"
+            placeholder="Enter your image URL"
+            required
+            value={edit?.imgurl}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Content</label>
+          <textarea
+            type="text"
+            className="form-control"
+            rows="6"
+            id="content"
+            placeholder="Enter your content"
+            required
+            value={edit?.content}
+            onChange={handleChange}
+          />
+        </div>
+        <input
+          type="submit"
+          className="btn btn-primary form-control"
+          value="Edit"
+        />
+      </form>
+    </div>
+  );
 }
 
-export default UpdateBlog
+export default UpdateBlog;
